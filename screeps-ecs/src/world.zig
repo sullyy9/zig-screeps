abda536e7dc954754bed53838a5ff417b73257f5
sullyy9 <ryansully96@googlemail.com>
2024-05-03 08:18:26 +0000

    Enable iterating over multiple different components at once

--- screeps-ecs/src/world.zig
+++ screeps-ecs/src/world.zig
@@ -10,6 +10,9 @@ const archetype = @import("archetype.zig");
 const ArchetypeTable = archetype.ArchetypeTable;
 const assertIsComponent = archetype.assertIsComponent;
 
+const query = @import("query.zig");
+const Query = query.Query;
+
 pub const EntityID = struct {
     id: usize,
 };
@@ -118,16 +121,21 @@ pub const World = struct {
         return table.getComponentMut(index.row, Component);
     }
 
-    pub fn iterComponent(self: *const Self, comptime Component: type) ComponentIter(Component) {
-        comptime assertIsComponent(Component);
-        return ComponentIter(Component).init(self.archetypes.items);
+    pub fn iter(self: *const Self, comptime Components: []const type) ComponentIter(Components) {
+        inline for (Components) |Component| {
+            comptime assertIsComponent(Component);
+        }
+
+        return ComponentIter(Components).init(self.archetypes.items);
     }
 
-    pub fn iterComponentMut(self: *Self, comptime Component: type) ComponentIterMut(Component) {
-        comptime assertIsComponent(Component);
-        return ComponentIterMut(Component).init(self.archetypes.items);
-    }
+    pub fn iterMut(self: *const Self, comptime Components: []const type) ComponentIterMut(Components) {
+        inline for (Components) |Component| {
+            comptime assertIsComponent(Component);
+        }
 
+        return ComponentIterMut(Components).init(self.archetypes.items);
+    }
 
     fn nextEntityID(self: *Self) EntityID {
         const id = EntityID{ .id = self.entity_count };
@@ -170,8 +178,25 @@ pub const World = struct {
     }
 };
 
-pub fn ComponentIter(comptime Component: type) type {
-    assertIsComponent(Component);
+pub fn ComponentIter(comptime Components: []const type) type {
+    comptime var Slices: [Components.len]type = undefined;
+    comptime var Pointers: [Components.len]type = undefined;
+
+    inline for (Components, 0..) |Component, i| {
+        assertIsComponent(Component);
+
+        Slices[i] = []const Component;
+        Pointers[i] = *const Component;
+    }
+
+    const ComponentSlices = Slices;
+    const ComponentPointers = Pointers;
+
+    const NextComponent = if (ComponentPointers.len == 1) brk: {
+        break :brk ComponentPointers[0];
+    } else brk: {
+        break :brk Tuple(&ComponentPointers);
+    };
 
     return struct {
         const Self = @This();
@@ -180,22 +205,31 @@ pub fn ComponentIter(comptime Component: type) type {
         tables: []const ArchetypeTable,
 
         row: usize,
-        column: []const Component,
+        columns: Tuple(&ComponentSlices),
 
         pub fn init(tables: []const ArchetypeTable) Self {
+            var columns: Tuple(&ComponentSlices) = undefined;
+            inline for (0..columns.len) |i| {
+                columns[i] = &.{};
+            }
+
             return Self{
                 .table = -1,
                 .row = 0,
                 .tables = tables,
-                .column = &.{},
+                .columns = columns,
             };
         }
 
-        pub fn next(self: *Self) ?*const Component {
-            if (self.row < self.column.len) {
-                const component = &self.column[self.row];
+        pub fn next(self: *Self) ?NextComponent {
+            if (self.row < self.columns[0].len) {
+                var components: Tuple(&ComponentPointers) = undefined;
+                inline for (0..self.columns.len) |i| {
+                    components[i] = &self.columns[i][self.row];
+                }
                 self.row += 1;
-                return component;
+
+                return if (components.len == 1) components[0] else components;
             }
 
             // If we've exhausted this table, move to the next one that isn't empty and contains
@@ -207,20 +241,46 @@ pub fn ComponentIter(comptime Component: type) type {
                 }
 
                 const table = self.tables[@intCast(self.table)];
-                if (table.hasComponent(Component) and table.rows() > 0) {
+                if (table.hasComponentsOf(Tuple(Components)) and table.rows() > 0) {
                     break;
                 }
             }
 
+            // Build the tuple of component columns from the next table.
             self.row = 1;
-            self.column = self.tables[@intCast(self.table)].getComponentSlice(Component);
-            return &self.column[0];
+            inline for (Components, 0..) |Component, i| {
+                self.columns[i] = self.tables[@intCast(self.table)].getComponentSlice(Component);
+            }
+
+            // Construct the return value.
+            var components: Tuple(&ComponentPointers) = undefined;
+            inline for (0..self.columns.len) |i| {
+                components[i] = &self.columns[i][0];
+            }
+            return if (components.len == 1) components[0] else components;
         }
     };
 }
 
-pub fn ComponentIterMut(comptime Component: type) type {
-    assertIsComponent(Component);
+pub fn ComponentIterMut(comptime Components: []const type) type {
+    comptime var Slices: [Components.len]type = undefined;
+    comptime var Pointers: [Components.len]type = undefined;
+
+    inline for (Components, 0..) |Component, i| {
+        assertIsComponent(Component);
+
+        Slices[i] = []Component;
+        Pointers[i] = *Component;
+    }
+
+    const ComponentSlices = Slices;
+    const ComponentPointers = Pointers;
+
+    const NextComponent = if (ComponentPointers.len == 1) brk: {
+        break :brk ComponentPointers[0];
+    } else brk: {
+        break :brk Tuple(&ComponentPointers);
+    };
 
     return struct {
         const Self = @This();
@@ -229,22 +289,32 @@ pub fn ComponentIterMut(comptime Component: type) type {
         tables: []ArchetypeTable,
 
         row: usize,
-        column: []Component,
+        columns: Tuple(&ComponentSlices),
 
         pub fn init(tables: []ArchetypeTable) Self {
+            var columns: Tuple(&ComponentSlices) = undefined;
+            inline for (0..columns.len) |i| {
+                columns[i] = &.{};
+            }
+
             return Self{
                 .table = -1,
                 .row = 0,
                 .tables = tables,
-                .column = &.{},
+                .columns = columns,
             };
         }
 
-        pub fn next(self: *Self) ?*Component {
-            if (self.row < self.column.len) {
-                const component = &self.column[self.row];
+        pub fn next(self: *Self) ?NextComponent {
+            if (self.row < self.columns[0].len) {
+                var components: Tuple(&ComponentPointers) = undefined;
+                inline for (0..self.columns.len) |i| {
+                    components[i] = &self.columns[i][self.row];
+                }
+
                 self.row += 1;
-                return component;
+
+                return if (components.len == 1) components[0] else components;
             }
 
             // If we've exhausted this table, move to the next one that isn't empty and contains
@@ -256,22 +326,32 @@ pub fn ComponentIterMut(comptime Component: type) type {
                 }
 
                 const table = self.tables[@intCast(self.table)];
-                if (table.hasComponent(Component) and table.rows() > 0) {
+                if (table.hasComponentsOf(Tuple(Components)) and table.rows() > 0) {
                     break;
                 }
             }
 
+            // Build the tuple of component columns from the next table.
             self.row = 1;
-            self.column = self.tables[@intCast(self.table)].getComponentSliceMut(Component);
-            return &self.column[0];
+            inline for (Components, 0..) |Component, i| {
+                self.columns[i] = self.tables[@intCast(self.table)].getComponentSliceMut(Component);
+            }
+
+            // Construct the return value.
+            var components: Tuple(&ComponentPointers) = undefined;
+            inline for (0..self.columns.len) |i| {
+                components[i] = &self.columns[i][0];
+            }
+            return if (components.len == 1) components[0] else components;
         }
     };
 }
 
-
 pub const Test = struct {
     const testing = std.testing;
     const allocator = std.testing.allocator;
+
+    const ArrayList = std.ArrayList;
 
     const Name = struct {
         const Self = @This();
@@ -280,6 +360,10 @@ pub const Test = struct {
 
         pub fn init(name: []const u8) Self {
             return Self{ .name = name };
+        }
+
+        pub fn order(lhs: Self, rhs: Self) std.math.Order {
+            return std.mem.order(u8, lhs.name, rhs.name);
         }
     };
 
@@ -291,6 +375,14 @@ pub const Test = struct {
 
         pub fn init(big: u64, smol: u8) Self {
             return Self{ .big_id = big, .smol_id = smol };
+        }
+
+        pub fn order(lhs: Self, rhs: Self) std.math.Order {
+            switch (std.math.order(lhs.big_id, rhs.big_id)) {
+                .eq => return std.math.order(lhs.smol_id, rhs.smol_id),
+                .gt => return .gt,
+                .lt => return .lt,
+            }
         }
     };
 
@@ -318,6 +410,14 @@ pub const Test = struct {
         pub fn init(name: Name, id: ID) Self {
             return Self{ .name = name, .id = id };
         }
+
+        pub fn order(lhs: Self, rhs: Self) std.math.Order {
+            switch (ID.order(lhs.id, rhs.id)) {
+                .eq => return Name.order(lhs.name, rhs.name),
+                .gt => return .gt,
+                .lt => return .lt,
+            }
+        }
     };
 
     const FunkyNameAndID = struct {
@@ -329,6 +429,12 @@ pub const Test = struct {
 
         pub fn init(name: Name, id: ID, funky: Funky) Self {
             return Self{ .name = name, .id = id, .funky = funky };
+        }
+    };
+
+    const NameSortCtx = struct {
+        fn lessThan(_: void, lhs: Name, rhs: Name) bool {
+            return Name.order(lhs, rhs) == .lt;
         }
     };
 
@@ -363,11 +469,11 @@ pub const Test = struct {
         try testing.expectEqual(Name.init("mutated"), mutated_name.*);
     }
 
-    test "iterComponent" {
+    test "iter" {
         var world = World.init(allocator);
         defer world.deinit();
 
-        const names: [5]Name = .{
+        var names = [_]Name{
             Name.init("test1"),
             Name.init("test2"),
             Name.init("test3"),
@@ -381,32 +487,23 @@ pub const Test = struct {
         _ = try world.newEntity(FunkyNameAndID.init(names[3], ID.init(69, 42), Funky.initInAGoodWay()));
         _ = try world.newEntity(FunkyNameAndID.init(names[4], ID.init(70, 43), Funky.initInABadWay()));
 
-        var name_iter = world.iterComponent(Name);
+        var iter = world.iter(&.{Name});
 
-        var seen: [names.len]bool = undefined;
-        @memset(&seen, false);
+        var collected = ArrayList(Name).init(allocator);
+        defer collected.deinit();
+        while (iter.next()) |name| try collected.append(name.*);
 
-        while (name_iter.next()) |name| {
-            var index: ?usize = null;
-            for (names, 0..) |expected_name, i| {
-                if (std.mem.eql(u8, name.name, expected_name.name)) {
-                    index = i;
-                    break;
-                }
-            }
+        std.sort.insertion(Name, &names, {}, NameSortCtx.lessThan);
+        std.sort.insertion(Name, collected.items, {}, NameSortCtx.lessThan);
 
-            try testing.expect(index != null);
-            seen[index.?] = true;
-        }
-
-        try testing.expect(std.mem.allEqual(bool, &seen, true));
+        try testing.expectEqualSlices(Name, &names, collected.items);
     }
 
-    test "iterComponentMut" {
+    test "iterMut" {
         var world = World.init(allocator);
         defer world.deinit();
 
-        const names: [5]Name = .{
+        var names = [_]Name{
             Name.init("test1"),
             Name.init("test2"),
             Name.init("test3"),
@@ -420,25 +517,143 @@ pub const Test = struct {
         _ = try world.newEntity(FunkyNameAndID.init(names[3], ID.init(69, 42), Funky.initInAGoodWay()));
         _ = try world.newEntity(FunkyNameAndID.init(names[4], ID.init(70, 43), Funky.initInABadWay()));
 
-        var name_iter = world.iterComponentMut(Name);
+        var iter = world.iterMut(&.{Name});
 
-        var seen: [names.len]bool = undefined;
+        var collected = ArrayList(Name).init(allocator);
+        defer collected.deinit();
+        while (iter.next()) |name| try collected.append(name.*);
+
+        std.sort.insertion(Name, &names, {}, NameSortCtx.lessThan);
+        std.sort.insertion(Name, collected.items, {}, NameSortCtx.lessThan);
+
+        try testing.expectEqualSlices(Name, &names, collected.items);
+    }
+
+    test "iter Multiple" {
+        var world = World.init(allocator);
+        defer world.deinit();
+
+        const names_and_ids: [5]Tuple(&.{ Name, ID }) = .{
+            .{ Name.init("test1"), ID.init(69, 42) },
+            .{ Name.init("test2"), ID.init(70, 43) },
+            .{ Name.init("test3"), ID.init(71, 44) },
+            .{ Name.init("test4"), ID.init(69, 42) },
+            .{ Name.init("test5"), ID.init(70, 43) },
+        };
+
+        _ = try world.newEntity(NameAndID.init(names_and_ids[0][0], names_and_ids[0][1]));
+        _ = try world.newEntity(NameAndID.init(names_and_ids[1][0], names_and_ids[1][1]));
+        _ = try world.newEntity(NameAndID.init(names_and_ids[2][0], names_and_ids[2][1]));
+        _ = try world.newEntity(FunkyNameAndID.init(names_and_ids[3][0], names_and_ids[3][1], Funky.initInAGoodWay()));
+        _ = try world.newEntity(FunkyNameAndID.init(names_and_ids[4][0], names_and_ids[4][1], Funky.initInABadWay()));
+
+        var name_iter = world.iter(&.{ Name, ID });
+
+        var seen: [names_and_ids.len]bool = undefined;
         @memset(&seen, false);
 
-        while (name_iter.next()) |name| {
-            var index: ?usize = null;
-            for (names, 0..) |expected_name, i| {
-                if (std.mem.eql(u8, name.name, expected_name.name)) {
-                    index = i;
+        while (name_iter.next()) |next| {
+            const name, const id = next;
+
+            for (names_and_ids, 0..) |expected, i| {
+                const expected_name, const expected_id = expected;
+
+                if (std.meta.eql(name.*, expected_name) and std.meta.eql(id.*, expected_id)) {
+                    try testing.expect(seen[i] == false);
+                    seen[i] = true;
                     break;
                 }
             }
-
-            try testing.expect(index != null);
-            seen[index.?] = true;
         }
 
         try testing.expect(std.mem.allEqual(bool, &seen, true));
     }
 
+    test "iterMut Multiple" {
+        var world = World.init(allocator);
+        defer world.deinit();
+
+        const names_and_ids: [5]Tuple(&.{ Name, ID }) = .{
+            .{ Name.init("test1"), ID.init(69, 42) },
+            .{ Name.init("test2"), ID.init(70, 43) },
+            .{ Name.init("test3"), ID.init(71, 44) },
+            .{ Name.init("test4"), ID.init(69, 42) },
+            .{ Name.init("test5"), ID.init(70, 43) },
+        };
+
+        _ = try world.newEntity(NameAndID.init(names_and_ids[0][0], names_and_ids[0][1]));
+        _ = try world.newEntity(NameAndID.init(names_and_ids[1][0], names_and_ids[1][1]));
+        _ = try world.newEntity(NameAndID.init(names_and_ids[2][0], names_and_ids[2][1]));
+        _ = try world.newEntity(FunkyNameAndID.init(names_and_ids[3][0], names_and_ids[3][1], Funky.initInAGoodWay()));
+        _ = try world.newEntity(FunkyNameAndID.init(names_and_ids[4][0], names_and_ids[4][1], Funky.initInABadWay()));
+
+        var name_iter = world.iterMut(&.{ Name, ID });
+
+        var seen: [names_and_ids.len]bool = undefined;
+        @memset(&seen, false);
+
+        while (name_iter.next()) |next| {
+            const name, const id = next;
+
+            for (names_and_ids, 0..) |expected, i| {
+                const expected_name, const expected_id = expected;
+
+                if (std.meta.eql(name.*, expected_name) and std.meta.eql(id.*, expected_id)) {
+                    try testing.expect(seen[i] == false);
+                    seen[i] = true;
+                    break;
+                }
+            }
+        }
+        try testing.expect(std.mem.allEqual(bool, &seen, true));
+    }
+
+    test "iterMut Mutation" {
+        var world = World.init(allocator);
+        defer world.deinit();
+
+        var names_and_ids: [5]Tuple(&.{ Name, ID }) = .{
+            .{ Name.init("test1"), ID.init(69, 42) },
+            .{ Name.init("test2"), ID.init(70, 43) },
+            .{ Name.init("test3"), ID.init(71, 44) },
+            .{ Name.init("test4"), ID.init(69, 42) },
+            .{ Name.init("test5"), ID.init(70, 43) },
+        };
+
+        _ = try world.newEntity(NameAndID.init(names_and_ids[0][0], names_and_ids[0][1]));
+        _ = try world.newEntity(NameAndID.init(names_and_ids[1][0], names_and_ids[1][1]));
+        _ = try world.newEntity(NameAndID.init(names_and_ids[2][0], names_and_ids[2][1]));
+        _ = try world.newEntity(FunkyNameAndID.init(names_and_ids[3][0], names_and_ids[3][1], Funky.initInAGoodWay()));
+        _ = try world.newEntity(FunkyNameAndID.init(names_and_ids[4][0], names_and_ids[4][1], Funky.initInABadWay()));
+
+        var iter = world.iterMut(&.{ Name, ID });
+        while (iter.next()) |next| {
+            _, const id = next;
+            id.big_id += 10;
+        }
+
+        for (&names_and_ids) |*item| {
+            const id = &item[1];
+            id.big_id += 10;
+        }
+
+        var seen: [names_and_ids.len]bool = undefined;
+        @memset(&seen, false);
+
+        iter = world.iterMut(&.{ Name, ID });
+        while (iter.next()) |next| {
+            const name, const id = next;
+
+            for (names_and_ids, 0..) |expected, i| {
+                const expected_name, const expected_id = expected;
+
+                if (std.meta.eql(name.*, expected_name) and std.meta.eql(id.*, expected_id)) {
+                    try testing.expect(seen[i] == false);
+                    seen[i] = true;
+                    break;
+                }
+            }
+        }
+        try testing.expect(std.mem.allEqual(bool, &seen, true));
+    }
 };
