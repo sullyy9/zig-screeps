a4837d7f4d841a7d60e007c081f2d9f9fb5d9f32
sullyy9 <ryansully96@googlemail.com>
2024-05-08 11:31:59 +0000

    Reorganised public interface

--- screeps-ecs/src/main.zig
+++ screeps-ecs/src/main.zig
@@ -1,15 +1,17 @@
-const std = @import("std");
-const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
+const ecs = @import("ecs.zig");
+const query = @import("query.zig");
+const world = @import("world.zig");
 
-const typeid = @import("typeid.zig");
-const typeID = typeid.typeID;
-
-pub const archetype = @import("archetype.zig");
-pub const world = @import("world.zig");
-pub const ecs = @import("ecs.zig");
-pub const query = @import("query.zig");
+pub const ECS = ecs.ECS;
+pub const Query = query.Query;
+pub const Systems = ecs.Systems;
+pub const EntityID = world.EntityID;
 
 test {
-    @import("std").testing.refAllDeclsRecursive(@This());
+    const std = @import("std");
+    std.testing.refAllDeclsRecursive(@import("ecs.zig"));
+    std.testing.refAllDeclsRecursive(@import("query.zig"));
+    std.testing.refAllDeclsRecursive(@import("world.zig"));
+    std.testing.refAllDeclsRecursive(@import("typeid.zig"));
+    std.testing.refAllDeclsRecursive(@import("archetype.zig"));
 }
