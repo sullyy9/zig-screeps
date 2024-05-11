2b4073f08dce350d9f619bb55625db6dfb5963b2
Ryan Sullivan <ryansully96@googlemail.com>
2024-05-11 09:47:10 +0000

    Fix ECS top level exports

--- screeps-ecs/src/main.zig
+++ screeps-ecs/src/main.zig
@@ -1,19 +1,16 @@
 const ecs = @import("ecs.zig");
 const query = @import("query.zig");
 const world = @import("world/mod.zig");
+const system = @import("system/mod.zig");
 
 pub const ECS = ecs.ECS;
 pub const Query = query.Query;
-pub const Systems = ecs.Systems;
+pub const SystemRegistry = system.Registry;
 pub const EntityID = world.EntityID;
 
 test {
     const std = @import("std");
     std.testing.refAllDeclsRecursive(@import("ecs.zig"));
     std.testing.refAllDeclsRecursive(@import("query.zig"));
-
     std.testing.refAllDeclsRecursive(@import("world/mod.zig"));
-    // std.testing.refAllDeclsRecursive(@import("world/world.zig"));
-    // std.testing.refAllDeclsRecursive(@import("world/typeid.zig"));
-    // std.testing.refAllDeclsRecursive(@import("world/archetype.zig"));
 }
